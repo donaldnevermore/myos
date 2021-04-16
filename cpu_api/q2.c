9f18cf8c046afa8f7ba693443a321729e7d6ce7e
Fujimiya D. Nevermore <donald33wang@gmail.com>
2021-04-16 21:36:06 +0800

    Update comment

--- cpu_api/q2.c
+++ cpu_api/q2.c
@@ -7,14 +7,13 @@ void run(void) {
     int fd = open("./q2_test.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
     int rc = fork();
 
-    // Why did it cause incorrect print message?
     write(fd, "hello\n", 6);
 
     if (rc == 0) {
         printf("hello, I am child (pid:%d)\n", (int)getpid());
     }
     else {
-        // Uncomment to fix it.
+        // Why is print message incorrect? Uncomment to fix it.
         // int rc_wait = wait(NULL);
         printf("hello, I am parent of %d (pid:%d)\n", rc, (int)getpid());
     }
