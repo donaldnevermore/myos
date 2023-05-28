179453d486bedb2c64bebeab30aad2ce59a921d5
Donald33 Wang <donald33wang@gmail.com>
2023-05-28 19:05:51 +0800

    Add p4

--- cpu_api/p4.c
+++ cpu_api/p4.c
@@ -6,24 +6,24 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    printf("hello world (pid:%d)\n", (int)getpid());
     int rc = fork();
     if (rc < 0) {
         // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
-    }
-    else if (rc == 0) {
+    } else if (rc == 0) {
+        // child: redirect standard output to a file
         close(STDOUT_FILENO);
         open("./p4.output", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
 
-        char *myargs[0];
+        // now exec "wc"...
+        char *myargs[3];
         myargs[0] = strdup("wc"); // word count
         myargs[1] = strdup("p4.c"); // file to count
         myargs[2] = NULL;
         execvp(myargs[0], myargs);
-    }
-    else {
+    } else {
+        // parent goes down this path (main)
         int rc_wait = wait(NULL);
     }
     return 0;
